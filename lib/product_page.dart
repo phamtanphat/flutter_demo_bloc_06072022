f34f4c3f56447b7fcf5b05700ae97d797a1893dd
phamtanphat <phatdroid94@gmail.com>
2022-08-22 18:30:17 +0700

    ui

--- lib/product_page.dart
+++ lib/product_page.dart
@@ -9,6 +9,31 @@ class ProductPage extends StatefulWidget {
 class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Bloc"),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+                onPressed: (){
+
+                },
+                child: Text("+")
+            ),
+            Text("0"),
+            ElevatedButton(
+                onPressed: (){
+
+                },
+                child: Text("-")
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
